ed8c200ca851527dcea2896238158ae6c752ccb4
Thomas Leen <tl@thomasleen.com>
2013-08-28 01:08:23 +0000

    changed previous fail test to pass

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -2,10 +2,10 @@ var main = require('../lib/main.js');
 
 describe("main tests", function(){
   it('should equal "Hello World"', function(){
-    expect(main.hello()).toEqual('Hello World');
+    expect(main.hello()).toBe('Hello World');
   });
 
   it('should also equal "Hello World"', function(){
-    expect(main.hello()).toEqual('Fail');
+    expect(main.hello()).not.toBe('Fail');
   });
 });
